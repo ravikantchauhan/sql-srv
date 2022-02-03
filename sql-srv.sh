4a10a8af0286d7d43427553e607d08151e6ffbd5
ravikantchauhan <ravi.aith@gmail.com>
2022-02-03 14:54:40 +0000

    error fix

--- sql-srv.sh
+++ sql-srv.sh
@@ -19,7 +19,7 @@ case $input in
             read php_versin
             ####
             sudo add-apt-repository -y ppa:ondrej/php
-            sudo apt-get update
+            #sudo apt-get update
             echo "Installing php$php_versin"
             sudo apt install -y php$php_versin
             sudo apt-get install -y $php_versin-{bcmath,bz2,intl,gd,mbstring,mysql,zip,common,dom}
@@ -29,47 +29,48 @@ case $input in
             ;;
       [nN][oO]|[nN])
             echo "You say No"
+            exit
             ;;
       *)
             echo "Invalid input..."
             exit 1
             ;;
 esac
-        exit
+       
 fi
 ######
 echo "pass to next line done"
-# sudo apt install software-properties-common
-# add-apt-repository ppa:ondrej/php -y
-# apt-get update
-# apt-get install php$PHP_V php$PHP_V-dev php$PHP_V-xml -y --allow-unauthenticated
-# ##########################
-# #Install the Microsoft ODBC driver for SQL
-# #Microsoft ODBC 17
+sudo apt install software-properties-common
+add-apt-repository ppa:ondrej/php -y
+apt-get update
+apt-get install php$PHP_V php$PHP_V-dev php$PHP_V-xml -y --allow-unauthenticated
+##########################
+#Install the Microsoft ODBC driver for SQL
+#Microsoft ODBC 17
 
-# ########
-# curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-# curl https://packages.microsoft.com/config/ubuntu/$os_version/prod.list > /etc/apt/sources.list.d/mssql-release.list
-# sudo apt-get update
-# sudo ACCEPT_EULA=Y apt-get install msodbcsql17
-# sudo apt-get install unixodbc-dev
-# ###########
-# sudo apt-get -y install php-pear php$PHP_V-dev
-# sudo update-alternatives --set php /usr/bin/php$PHP_V
-# sudo update-alternatives --set phar /usr/bin/phar$PHP_V
-# sudo update-alternatives --set phar.phar /usr/bin/phar.phar$PHP_V
-# sudo update-alternatives --set phpize /usr/bin/phpize$PHP_V
-# sudo update-alternatives --set php-config /usr/bin/php-config$PHP_V
-# ##########
-# ######sqlsrv
-# sudo pecl uninstall -r sqlsrv
-# sudo pecl uninstall -r pdo_sqlsrv
-# sudo pecl -d php_suffix=$PHP_V install sqlsrv
-# sudo pecl -d php_suffix=$PHP_V install pdo_sqlsrv
-# #####
-# printf "; priority=20\nextension=sqlsrv.so\n" > /etc/php/$PHP_V/mods-available/sqlsrv.ini
-# printf "; priority=30\nextension=pdo_sqlsrv.so\n" > /etc/php/$PHP_V/mods-available/pdo_sqlsrv.ini
-# sudo phpenmod -v $PHP_V sqlsrv pdo_sqlsrv
-# sudo service apache2 restart
-# echo "Done Pleash check"
-# #########
+########
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/$os_version/prod.list > /etc/apt/sources.list.d/mssql-release.list
+sudo apt-get update
+sudo ACCEPT_EULA=Y apt-get install msodbcsql17
+sudo apt-get install unixodbc-dev
+###########
+sudo apt-get -y install php-pear php$PHP_V-dev
+sudo update-alternatives --set php /usr/bin/php$PHP_V
+sudo update-alternatives --set phar /usr/bin/phar$PHP_V
+sudo update-alternatives --set phar.phar /usr/bin/phar.phar$PHP_V
+sudo update-alternatives --set phpize /usr/bin/phpize$PHP_V
+sudo update-alternatives --set php-config /usr/bin/php-config$PHP_V
+##########
+######sqlsrv
+sudo pecl uninstall -r sqlsrv
+sudo pecl uninstall -r pdo_sqlsrv
+sudo pecl -d php_suffix=$PHP_V install sqlsrv
+sudo pecl -d php_suffix=$PHP_V install pdo_sqlsrv
+#####
+printf "; priority=20\nextension=sqlsrv.so\n" > /etc/php/$PHP_V/mods-available/sqlsrv.ini
+printf "; priority=30\nextension=pdo_sqlsrv.so\n" > /etc/php/$PHP_V/mods-available/pdo_sqlsrv.ini
+sudo phpenmod -v $PHP_V sqlsrv pdo_sqlsrv
+sudo service apache2 restart
+echo "Done Pleash check"
+#########
